f5e6a652949ccce9323095ee13bfebdfc83f857b
Jelena Adamović <105116909+adamoviccj@users.noreply.github.com>
2023-01-31 19:58:35 +0100

    Test za MemTable

---
+++ structures/testMemTable.go
@@ -0,0 +1,35 @@
+package structures
+
+import "fmt"
+
+func MemTableTest() {
+	mt := NewMemTable(3, 3)
+	mt.AddNew("Jelena")
+	mt.AddNew("Marija")
+	mt.AddNew("test")
+
+	ok, _, _ := mt.Find("Jelena")
+	fmt.Println(ok)
+	ok2, _, _ := mt.Find("Aleksandar")
+	fmt.Println(ok2)
+	ok3, _, _ := mt.Find("test")
+	fmt.Println(ok3)
+
+	mt.Remove("test")
+
+	deleted, item, _ := mt.Find("test")
+	fmt.Println(deleted)
+	fmt.Println(item)
+
+	mt.Edit("Jelena", "123")
+
+	ok4, item, _ := mt.Find("Jelena")
+	fmt.Println(ok4)
+	fmt.Println(item)
+
+	fmt.Println(mt.size)
+
+	ok5 := mt.checkFlush()
+	fmt.Println(ok5)
+
+}
