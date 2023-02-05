b6742b1ff6a70b1d13e6bb0c45755947752a35a7
borismarkovsv73 <markovboris3@gmail.com>
2023-02-05 20:19:58 +0100

    Update testMemTable.go

--- structures/testMemTable.go
+++ structures/testMemTable.go
@@ -1,38 +1,38 @@
 package structures
 
-import "fmt"
+// import "fmt"
 
 // Jelena Adamovic, SV 6/2021
 
 // test za Memtable
 func MemTableTest() {
-	mt := NewMemTable(3, 3, 32)
-	mt.AddNew("Jelena")
-	mt.AddNew("Marija")
-	mt.AddNew("test")
+	// mt := NewMemTable(3, 3, 32)
+	// mt.AddNew("Jelena")
+	// mt.AddNew("Marija")
+	// mt.AddNew("test")
 
-	ok, _, _ := mt.Find("Jelena")
-	fmt.Println(ok)
-	ok2, _, _ := mt.Find("Aleksandar")
-	fmt.Println(ok2)
-	ok3, _, _ := mt.Find("test")
-	fmt.Println(ok3)
+	// ok, _, _ := mt.Find("Jelena")
+	// fmt.Println(ok)
+	// ok2, _, _ := mt.Find("Aleksandar")
+	// fmt.Println(ok2)
+	// ok3, _, _ := mt.Find("test")
+	// fmt.Println(ok3)
 
-	mt.Remove("test")
+	// mt.Remove("test")
 
-	deleted, item, _ := mt.Find("test")
-	fmt.Println(deleted)
-	fmt.Println(item)
+	// deleted, item, _ := mt.Find("test")
+	// fmt.Println(deleted)
+	// fmt.Println(item)
 
-	mt.Edit("Jelena", "123")
+	// mt.Edit("Jelena", "123")
 
-	ok4, item, _ := mt.Find("Jelena")
-	fmt.Println(ok4)
-	fmt.Println(item)
+	// ok4, item, _ := mt.Find("Jelena")
+	// fmt.Println(ok4)
+	// fmt.Println(item)
 
-	fmt.Println(mt.size)
+	// fmt.Println(mt.size)
 
-	ok5 := mt.checkFlush()
-	fmt.Println(ok5)
+	// ok5 := mt.checkFlush()
+	// fmt.Println(ok5)
 
 }
