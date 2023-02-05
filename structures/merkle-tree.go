dc732f6ba6d0aa69b9f6ba054e6d76ad35004afb
marijazhiv <105136036+marijazhiv@users.noreply.github.com>
2023-02-05 22:12:10 +0100

    Update merkle-tree.go

--- structures/merkle-tree.go
+++ structures/merkle-tree.go
@@ -64,7 +64,7 @@ func CreateMerkleTree(keys [][]byte, path string) *MerkleRoot {
 
     root := MerkleRoot{root_node}
     new_path := strings.Replace(path, "Data.db", "Metadata.txt", 1)
-    new_path = "./kv-system/data/metadata/" + new_path
+    new_path = "data/metadata/" + new_path
     WriteInFile(root_node, new_path)
     return &root
 }
